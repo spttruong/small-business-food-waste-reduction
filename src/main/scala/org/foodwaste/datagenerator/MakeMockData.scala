05631b2b320d89e95449694333b451644d7654e1
spttruong <spttruong@gmail.com>
2022-05-05 13:06:24 -0700

    Updated MakeMockData.scala to output a named .csv file with help of spark.daria package

--- src/main/scala/org/foodwaste/datagenerator/MakeMockData.scala
+++ src/main/scala/org/foodwaste/datagenerator/MakeMockData.scala
@@ -5,8 +5,9 @@ import org.foodwaste.datagenerator.MockDataGenerator.FoodWasteRecord
 // import com.typesafe.scalalogging.{LazyLogging}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.{Dataset}
+import org.apache.spark.sql.{DataFrame}
 import org.foodwaste.utils.SparkUtils
-
+import com.github.mrpowers.spark.daria.sql.DariaWriters.writeSingleFile
 
 object MakeMockData {
  
@@ -17,8 +18,8 @@ object MakeMockData {
         // spark instance
         val spark = SparkUtils.sparkSession("spark-food-waste-records-generator", "local[*]")
         // convert data to DF and save as .CSV
-        val generatedDataDF = listToDs(spark)(generatedData)
-        DfToCsv(generatedDataDF)
+        val generatedDataDF = listToDf(spark)(generatedData)
+        DfToCsv(spark)(generatedDataDF)
         spark.stop()
     }
 
@@ -27,11 +28,26 @@ object MakeMockData {
 
         val dataRDD = spark.sparkContext.parallelize(dataList)
         val ds = dataRDD.toDS()
-        ds.show()
         ds
     }
 
-    def DfToCsv(ds: Dataset[FoodWasteRecord]): Unit = {
+    def listToDf(spark: SparkSession)(dataList: List[FoodWasteRecord]): DataFrame = {
+        import spark.implicits._
+        val dataRDD = spark.sparkContext.parallelize(dataList)
+        val df = dataRDD.toDF()
+        df
+    }
+
+    def DsToCsv(ds: Dataset[FoodWasteRecord]): Unit = {
         ds.coalesce(1).write.csv("src/main/resources/sample-food-waste-records/")
+    }
+
+    def DfToCsv(spark: SparkSession)(df: DataFrame): Unit = {
+        writeSingleFile(
+            df = df,
+            format = "csv",
+            sc = spark.sparkContext,
+            tmpFolder = "src/main/resources/mock_data_output/",
+            filename = "src/main/resources/mock_data_output/mockdata.csv")
     }
 }
